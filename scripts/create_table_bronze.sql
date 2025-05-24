c5ef7fa01d1bd03a0add227df67f1519bb4faf65
OhmSaharath <107943284+OhmSaharath@users.noreply.github.com>
2025-05-25 01:34:16 +0700

    Update create_table_bronze.sql

--- scripts/create_table_bronze.sql
+++ scripts/create_table_bronze.sql
@@ -4,13 +4,13 @@ IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_cust_info;
 -- CRM_CUST_INFO
 CREATE TABLE bronze.crm_cust_info (
-	cst_id				      INT,
-	cst_key				      NVARCHAR(50),
-	cst_firstname		    NVARCHAR(50),
-	cst_lastname		    NVARCHAR(50),
-	cst_material_status NVARCHAR(50),
-	cst_gndr			      NVARCHAR(50),
-	cst_create_data		  DATE
+	cst_id			INT,
+	cst_key			NVARCHAR(50),
+	cst_firstname		NVARCHAR(50),
+	cst_lastname		NVARCHAR(50),
+	cst_material_status 	NVARCHAR(50),
+	cst_gndr		NVARCHAR(50),
+	cst_create_data		DATE
 );
 
 -- DROP AND RECREATE
@@ -18,12 +18,12 @@ IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_prd_info;
 -- CRM_PRD_INFO
 CREATE TABLE bronze.crm_prd_info(
-	prd_id			  INT,
-	prd_key			  NVARCHAR(50),
-	prd_nm			  NVARCHAR(50),
-	prd_cost		  INT,
-	prd_line		  NVARCHAR(50),
-	prd_start_dt	DATETIME,
+	prd_id			INT,
+	prd_key			NVARCHAR(50),
+	prd_nm			NVARCHAR(50),
+	prd_cost		INT,
+	prd_line		NVARCHAR(50),
+	prd_start_dt		DATETIME,
 	prd_end_dt		DATETIME
 );
 -- DROP AND RECREATE
@@ -34,12 +34,12 @@ CREATE TABLE bronze.crm_sales_details (
 	sls_ord_num		NVARCHAR(50),
 	sls_prd_key		NVARCHAR(50),
 	sls_cust_id		INT,
-	sls_order_dt	INT,
+	sls_order_dt		INT,
 	sls_ship_dt		INT,
 	sls_due_dt		INT,
-	sls_sales		  INT,
-	sls_quantity	INT,
-	sls_price		  INT
+	sls_sales		INT,
+	sls_quantity		INT,
+	sls_price		INT
 );
 
 -- DROP AND RECREATE
@@ -48,7 +48,7 @@ IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
 -- ERP_CUST_AZ12
 CREATE TABLE bronze.erp_cust_az12 (
 	cid		NVARCHAR(50),
-	bdate	DATE,
+	bdate		DATE,
 	gen		NVARCHAR(50)
 );
 
@@ -58,7 +58,7 @@ IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
 -- ERP_LOC_A101
 CREATE TABLE bronze.erp_loc_a101(
 	cid		NVARCHAR(50),
-	cntry	NVARCHAR(50),
+	cntry		NVARCHAR(50)
 );
 
 -- DROP AND RECREATE
@@ -66,9 +66,9 @@ IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_px_cat_g1v2;
 -- ERP_PX_CAT_G1V2
 CREATE TABLE bronze.erp_px_cat_g1v2 (
-	id			    NVARCHAR(50),
-	cat			    NVARCHAR(50),
-	subcat		  NVARCHAR(50),
-	maintenance NVARCHAR(50)
+	id			NVARCHAR(50),
+	cat			NVARCHAR(50),
+	subcat			NVARCHAR(50),
+	maintenance 		NVARCHAR(50)
 );
 
